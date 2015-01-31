5626a56980eaf5b3abdd52bfa001d6de15a69948
Niklas Klein <my.taig@gmail.com>
2015-01-31 03:13:39 +0100

    Auto format

--- src/main/java/com/taig/android/barcode/scanner/DisplayUtils.java
+++ src/main/java/com/taig/android/barcode/scanner/DisplayUtils.java
@@ -6,36 +6,46 @@ import android.graphics.Point;
 import android.view.Display;
 import android.view.WindowManager;
 
-public class DisplayUtils {
-    public static Point getScreenResolution(Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        Point screenResolution = new Point();
-        if (android.os.Build.VERSION.SDK_INT >= 13) {
-            display.getSize(screenResolution);
-        } else {
-            screenResolution.set(display.getWidth(), display.getHeight());
-        }
+public class DisplayUtils
+{
+	public static Point getScreenResolution( Context context )
+	{
+		WindowManager wm = (WindowManager) context.getSystemService( Context.WINDOW_SERVICE );
+		Display display = wm.getDefaultDisplay();
+		Point screenResolution = new Point();
+		if( android.os.Build.VERSION.SDK_INT >= 13 )
+		{
+			display.getSize( screenResolution );
+		}
+		else
+		{
+			screenResolution.set( display.getWidth(), display.getHeight() );
+		}
 
-        return screenResolution;
-    }
+		return screenResolution;
+	}
 
-    public static int getScreenOrientation(Context context)
-    {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
+	public static int getScreenOrientation( Context context )
+	{
+		WindowManager wm = (WindowManager) context.getSystemService( Context.WINDOW_SERVICE );
+		Display display = wm.getDefaultDisplay();
 
-        int orientation = Configuration.ORIENTATION_UNDEFINED;
-        if(display.getWidth()==display.getHeight()){
-            orientation = Configuration.ORIENTATION_SQUARE;
-        } else{
-            if(display.getWidth() < display.getHeight()){
-                orientation = Configuration.ORIENTATION_PORTRAIT;
-            }else {
-                orientation = Configuration.ORIENTATION_LANDSCAPE;
-            }
-        }
-        return orientation;
-    }
-
+		int orientation = Configuration.ORIENTATION_UNDEFINED;
+		if( display.getWidth() == display.getHeight() )
+		{
+			orientation = Configuration.ORIENTATION_SQUARE;
+		}
+		else
+		{
+			if( display.getWidth() < display.getHeight() )
+			{
+				orientation = Configuration.ORIENTATION_PORTRAIT;
+			}
+			else
+			{
+				orientation = Configuration.ORIENTATION_LANDSCAPE;
+			}
+		}
+		return orientation;
+	}
 }
--- src/main/java/com/taig/android/barcode/scanner/ZXingScannerView.java
+++ src/main/java/com/taig/android/barcode/scanner/ZXingScannerView.java
@@ -24,126 +24,158 @@ import java.util.Map;
 import me.dm7.barcodescanner.core.BarcodeScannerView;
 import me.dm7.barcodescanner.core.DisplayUtils;
 
-public class ZXingScannerView extends BarcodeScannerView {
-    public interface ResultHandler {
-        public void handleResult(Result rawResult);
-    }
+public class ZXingScannerView extends BarcodeScannerView
+{
+	public interface ResultHandler
+	{
+		public void handleResult( Result rawResult );
+	}
 
-    private MultiFormatReader mMultiFormatReader;
-    public static final List<BarcodeFormat> ALL_FORMATS = new ArrayList<BarcodeFormat>();
-    private List<BarcodeFormat> mFormats;
-    private ResultHandler mResultHandler;
+	private MultiFormatReader mMultiFormatReader;
+	public static final List<BarcodeFormat> ALL_FORMATS = new ArrayList<BarcodeFormat>();
+	private List<BarcodeFormat> mFormats;
+	private ResultHandler mResultHandler;
 
-    static {
-        ALL_FORMATS.add(BarcodeFormat.UPC_A);
-        ALL_FORMATS.add(BarcodeFormat.UPC_E);
-        ALL_FORMATS.add(BarcodeFormat.EAN_13);
-        ALL_FORMATS.add(BarcodeFormat.EAN_8);
-        ALL_FORMATS.add(BarcodeFormat.RSS_14);
-        ALL_FORMATS.add(BarcodeFormat.CODE_39);
-        ALL_FORMATS.add(BarcodeFormat.CODE_93);
-        ALL_FORMATS.add(BarcodeFormat.CODE_128);
-        ALL_FORMATS.add(BarcodeFormat.ITF);
-        ALL_FORMATS.add(BarcodeFormat.CODABAR);
-        ALL_FORMATS.add(BarcodeFormat.QR_CODE);
-        ALL_FORMATS.add(BarcodeFormat.DATA_MATRIX);
-        ALL_FORMATS.add(BarcodeFormat.PDF_417);
-    }
+	static
+	{
+		ALL_FORMATS.add( BarcodeFormat.UPC_A );
+		ALL_FORMATS.add( BarcodeFormat.UPC_E );
+		ALL_FORMATS.add( BarcodeFormat.EAN_13 );
+		ALL_FORMATS.add( BarcodeFormat.EAN_8 );
+		ALL_FORMATS.add( BarcodeFormat.RSS_14 );
+		ALL_FORMATS.add( BarcodeFormat.CODE_39 );
+		ALL_FORMATS.add( BarcodeFormat.CODE_93 );
+		ALL_FORMATS.add( BarcodeFormat.CODE_128 );
+		ALL_FORMATS.add( BarcodeFormat.ITF );
+		ALL_FORMATS.add( BarcodeFormat.CODABAR );
+		ALL_FORMATS.add( BarcodeFormat.QR_CODE );
+		ALL_FORMATS.add( BarcodeFormat.DATA_MATRIX );
+		ALL_FORMATS.add( BarcodeFormat.PDF_417 );
+	}
 
-    public ZXingScannerView(Context context) {
-        super(context);
-        initMultiFormatReader();
-    }
+	public ZXingScannerView( Context context )
+	{
+		super( context );
+		initMultiFormatReader();
+	}
 
-    public ZXingScannerView(Context context, AttributeSet attributeSet) {
-        super(context, attributeSet);
-        initMultiFormatReader();
-    }
+	public ZXingScannerView( Context context, AttributeSet attributeSet )
+	{
+		super( context, attributeSet );
+		initMultiFormatReader();
+	}
 
-    public void setFormats(List<BarcodeFormat> formats) {
-        mFormats = formats;
-        initMultiFormatReader();
-    }
+	public void setFormats( List<BarcodeFormat> formats )
+	{
+		mFormats = formats;
+		initMultiFormatReader();
+	}
 
-    public void setResultHandler(ResultHandler resultHandler) {
-        mResultHandler = resultHandler;
-    }
+	public void setResultHandler( ResultHandler resultHandler )
+	{
+		mResultHandler = resultHandler;
+	}
 
-    public Collection<BarcodeFormat> getFormats() {
-        if(mFormats == null) {
-            return ALL_FORMATS;
-        }
-        return mFormats;
-    }
+	public Collection<BarcodeFormat> getFormats()
+	{
+		if( mFormats == null )
+		{
+			return ALL_FORMATS;
+		}
+		return mFormats;
+	}
 
-    private void initMultiFormatReader() {
-        Map<DecodeHintType,Object> hints = new EnumMap<DecodeHintType,Object>(DecodeHintType.class);
-        hints.put(DecodeHintType.POSSIBLE_FORMATS, getFormats());
-        mMultiFormatReader = new MultiFormatReader();
-        mMultiFormatReader.setHints(hints);
-    }
+	private void initMultiFormatReader()
+	{
+		Map<DecodeHintType, Object> hints = new EnumMap<DecodeHintType, Object>( DecodeHintType.class );
+		hints.put( DecodeHintType.POSSIBLE_FORMATS, getFormats() );
+		mMultiFormatReader = new MultiFormatReader();
+		mMultiFormatReader.setHints( hints );
+	}
 
-    @Override
-    public void onPreviewFrame(byte[] data, Camera camera) {
-        Camera.Parameters parameters = camera.getParameters();
-        Camera.Size size = parameters.getPreviewSize();
-        int width = size.width;
-        int height = size.height;
+	@Override
+	public void onPreviewFrame( byte[] data, Camera camera )
+	{
+		Camera.Parameters parameters = camera.getParameters();
+		Camera.Size size = parameters.getPreviewSize();
+		int width = size.width;
+		int height = size.height;
 
-        if(DisplayUtils.getScreenOrientation(getContext()) == Configuration.ORIENTATION_PORTRAIT) {
-            byte[] rotatedData = new byte[data.length];
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++)
-                    rotatedData[x * height + height - y - 1] = data[x + y * width];
-            }
-            int tmp = width;
-            width = height;
-            height = tmp;
-            data = rotatedData;
-        }
+		if( DisplayUtils.getScreenOrientation( getContext() ) == Configuration.ORIENTATION_PORTRAIT )
+		{
+			byte[] rotatedData = new byte[data.length];
+			for( int y = 0; y < height; y++ )
+			{
+				for( int x = 0; x < width; x++ )
+					rotatedData[x * height + height - y - 1] = data[x + y * width];
+			}
+			int tmp = width;
+			width = height;
+			height = tmp;
+			data = rotatedData;
+		}
 
-        Result rawResult = null;
-        PlanarYUVLuminanceSource source = buildLuminanceSource(data, width, height);
+		Result rawResult = null;
+		PlanarYUVLuminanceSource source = buildLuminanceSource( data, width, height );
 
-        if(source != null) {
-            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-            try {
-                rawResult = mMultiFormatReader.decodeWithState(bitmap);
-            } catch (ReaderException re) {
-                // continue
-            } catch (NullPointerException npe) {
-                // This is terrible
-            } catch (ArrayIndexOutOfBoundsException aoe) {
+		if( source != null )
+		{
+			BinaryBitmap bitmap = new BinaryBitmap( new HybridBinarizer( source ) );
+			try
+			{
+				rawResult = mMultiFormatReader.decodeWithState( bitmap );
+			}
+			catch( ReaderException re )
+			{
+				// continue
+			}
+			catch( NullPointerException npe )
+			{
+				// This is terrible
+			}
+			catch( ArrayIndexOutOfBoundsException aoe )
+			{
 
-            } finally {
-                mMultiFormatReader.reset();
-            }
-        }
+			}
+			finally
+			{
+				mMultiFormatReader.reset();
+			}
+		}
 
-        if (rawResult != null) {
-            stopCamera();
-            if(mResultHandler != null) {
-                mResultHandler.handleResult(rawResult);
-            }
-        } else {
-            camera.setOneShotPreviewCallback(this);
-        }
-    }
+		if( rawResult != null )
+		{
+			stopCamera();
+			if( mResultHandler != null )
+			{
+				mResultHandler.handleResult( rawResult );
+			}
+		}
+		else
+		{
+			camera.setOneShotPreviewCallback( this );
+		}
+	}
 
-    public PlanarYUVLuminanceSource buildLuminanceSource(byte[] data, int width, int height) {
-        Rect rect = getFramingRectInPreview(width, height);
-        if (rect == null) {
-            return null;
-        }
-        // Go ahead and assume it's YUV rather than die.
-        PlanarYUVLuminanceSource source = null;
+	public PlanarYUVLuminanceSource buildLuminanceSource( byte[] data, int width, int height )
+	{
+		Rect rect = getFramingRectInPreview( width, height );
+		if( rect == null )
+		{
+			return null;
+		}
+		// Go ahead and assume it's YUV rather than die.
+		PlanarYUVLuminanceSource source = null;
 
-        try {
-            source = new PlanarYUVLuminanceSource(data, width, height, rect.left, rect.top,
-                    rect.width(), rect.height(), false);
-        } catch(Exception e) {
-        }
+		try
+		{
+			source = new PlanarYUVLuminanceSource( data, width, height, rect.left, rect.top,
+				rect.width(), rect.height(), false );
+		}
+		catch( Exception e )
+		{
+		}
 
-        return source;
-    }
+		return source;
+	}
 }
